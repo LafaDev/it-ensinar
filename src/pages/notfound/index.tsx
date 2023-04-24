bd67e4b87fd4b6a58c40f94399ae5cfed0cc1d8a
lafadev <lafa.dev@protonmail.com>
2023-04-24 14:08:14 -0300

    feat: not found page for inexistent routes

---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notfound: React.FC = () => {
+  return (
+    <>
+      <p>Notfound</p>
+    </>
+  )
+}
+
+export default Notfound;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/dashboard";
+import Notfound from "../pages/notfound";
 
 const RouterComponent: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="*" element={<Notfound />} />
       </Routes>
     </BrowserRouter>
   )
